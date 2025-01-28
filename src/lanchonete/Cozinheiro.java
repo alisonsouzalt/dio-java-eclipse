9afcd7f3ecddc01d13c26c2b2614d9779d99a22b
alisonsouzalt <you@example.com>
2025-01-28 17:08:19 -0300

    upp

--- src/lanchonete/Cozinheiro.java
+++ src/lanchonete/Cozinheiro.java
@@ -1,5 +1,14 @@
 package lanchonete;
 
 public class Cozinheiro {
-
+	//pode ser default
+	
+	public void adicionarLancheNoBalcao() {
+		System.out.println("ADICIONANDO LANCHE NATURAL HAMBURGER NO BALCAO");
+	}
+	//pode ser default
+	public void adicionarSucoNoBalcao() {
+		System.out.println("ADICIONANDO SUCO NO BALCAO");
+	}
+	
 }
